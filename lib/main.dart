80b0723be5627beecc9cc374c91c76d1a5c84aa1
LAPH <laph@MacBook-Pro-de-Luis.local>
2025-07-31 11:30:10 -0600

    Updates

--- lib/main.dart
+++ lib/main.dart
@@ -9,68 +9,36 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Página en Mantenimiento',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MaintenancePage(),
-    );
-  }
-}
-
-class MaintenancePage extends StatelessWidget {
-  const MaintenancePage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey[200],
       body: Center(
-        child: Container(
-          padding: EdgeInsets.all(20),
-          decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(10),
-            boxShadow: [
-              BoxShadow(
-                color: Colors.black26,
-                blurRadius: 10,
-                offset: Offset(0, 4),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(
+              ':(',
+              style: TextStyle(
+                fontSize: 100,
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
               ),
-            ],
-          ),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: <Widget>[
-              Icon(
-                Icons.build, // Icono de herramientas, representando mantenimiento
-                size: 80,
-                color: Colors.orange,
+            ),
+            Text(
+              'Bajo construcción',
+              style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
               ),
-              SizedBox(height: 20),
-              Text(
-                'Página en Mantenimiento',
-                style: TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.redAccent,
-                ),
+            ),
+            Text(
+              'Nuestro citio web está en construción, estamos \n trabajando muy duro para brindarle la mejor experiencia \n pronto estará disponible.',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+                color: Colors.redAccent,
               ),
-              SizedBox(height: 15),
-              Text(
-                'Estamos trabajando en mejoras. Vuelve pronto.',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 16,
-                  color: Colors.grey[600],
-                ),
-              ),
-              SizedBox(height: 30),
-              CircularProgressIndicator(), // Indicador de carga
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
